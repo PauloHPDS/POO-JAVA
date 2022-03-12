34bd4cdb80a5c27a02f29b5ec72a06686eb462a8
Paulo Henrique <96488452+PauloHPDS@users.noreply.github.com>
2022-03-12 10:12:18 -0300

    Avaliação 1
    colocando questão inteira

--- Exercicios/Avaliacao1.java
+++ Exercicios/Avaliacao1.java
@@ -4,7 +4,9 @@ public class Avaliacao1 {
 	
 	public static void main(String[] args) {
 		
-		// Calcula o aumento salarial anualmente. dobrando a cada ano.
+		/* Um Empregado de uma empresa recebe aumento salarial anualmente. Sabe-se que esse empregado foi contratado em 2019, com salário inicial de R$ 1.000,00,
+		em 2020 recebeu aumento de 1,5% sobre seu salário inicial, a partir de 2021 (inclusive), os aumentos salariais sempre corresponderam ao dobro do percentual 
+		do ano anterior.Faça um programa em java que determine o salário desse empregado até o ano de 2026. E exiba todos os salários na tela.*/
 		
 		double salario=1000.0,aumento=1.5;
 		int ano=2019;
